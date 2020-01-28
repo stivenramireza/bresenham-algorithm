b88570a89c7f9493b2b5cbba3114755b12ede713
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-01-28 18:40:20 -0500

    Adding Bresenham Algorithm

---
+++ src/main/java/BresenhamAlgorithm/Main.java
@@ -0,0 +1,18 @@
+package BresenhamAlgorithm;
+
+/**
+ *
+ * Implementation of Bresenham Algorithm.
+ * @author stivenramireza
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bresenham Algorithm");
+    }
+    
+}
